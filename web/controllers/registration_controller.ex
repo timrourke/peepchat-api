ae3fd63cebf88ea4339d793568309a0f3396bbd5
Tim Rourke <tim@timrourke.com>
2016-05-22 11:22:34 -0500

    Fixing bug in registration controller

--- web/controllers/registration_controller.ex
+++ web/controllers/registration_controller.ex
@@ -1,6 +1,8 @@
 defmodule Peepchat.RegistrationController do
 	use Peepchat.Web, :controller
 
+	alias Peepchat.User
+
 	def create(conn, %{
 		"data" => %{	
 			"type" => "user",
@@ -11,7 +13,23 @@ defmodule Peepchat.RegistrationController do
 			},
 		}
 	}) do
-		conn
-		|> json(%{status: "Ok"})
+
+		changeset = User.changeset %User{}, %{
+			email: email,
+			password_confirmation: password_confirmation,
+			password: password
+		}
+
+		case Repo.insert changeset do
+			{:ok, user} ->
+				conn
+				|> put_status(:created)
+				|> render(Peepchat.UserView, "show.json", user: user)
+			{:error, changeset} ->
+				conn
+				|> put_status(:unprocessable_entity)
+				|> render(Peepchat.ChangesetView, "error.json", changeset: changeset)
+		end
+	
 	end
 end
