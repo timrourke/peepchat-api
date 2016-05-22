02d27e29514eaaba36a938f515b301f383aa9cb5
Tim Rourke <tim@timrourke.com>
2016-05-22 08:10:35 -0500

    Initial configuration.

---
+++ config/prod.secret.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :peepchat, Peepchat.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :peepchat, Peepchat.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("DATABASE_USER"),
+  password: System.get_env("DATABASE_PASSWORD"),
+  database: System.get_env("DATABASE_DBNAME"),
+  pool_size: 20
