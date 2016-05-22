08cf5de59f048944db1f5c29c3f0d3d0dbc4670b
Tim Rourke <tim@timrourke.com>
2016-05-22 11:09:45 -0500

    Roughing out registration controller and tests

--- web/router.ex
+++ web/router.ex
@@ -8,6 +8,9 @@ defmodule Peepchat.Router do
   scope "/api", Peepchat do
     pipe_through :api
 
+    # New user registration
+    post "register", RegistrationController, :create
+
     resources "session", SessionController, only: [:index]
   end
 end
